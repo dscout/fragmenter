da91a96c2b4f28192e93186ff011eccf3faa3696
Parker Selbert <parker@sorentwo.com>
2013-07-22 10:51:25 -0500

    Update expectation syntax for wrapper specs

--- spec/fragmenter/wrapper_spec.rb
+++ spec/fragmenter/wrapper_spec.rb
@@ -5,11 +5,13 @@ describe Fragmenter::Wrapper do
   let(:engine_class) { double(:engine_class, new: engine) }
   let(:engine)       { double(:engine) }
 
-  subject(:base) { Fragmenter::Wrapper.new(object, engine_class) }
+  subject(:wrapper) do
+    Fragmenter::Wrapper.new(object, engine_class)
+  end
 
   describe '#key' do
     it 'composes a key from the object class and id value' do
-      base.key.should match(/[a-z]+-\d+/)
+      expect(wrapper.key).to match(/[a-z]+-\d+/)
     end
   end
 
@@ -18,14 +20,14 @@ describe Fragmenter::Wrapper do
     let(:headers) { {} }
 
     it 'delegates #store to the storage engine' do
-      engine.should_receive(:store).with(blob, headers)
+      expect(engine).to receive(:store).with(blob, headers)
 
-      base.store(blob, headers)
+      wrapper.store(blob, headers)
     end
 
     it 'delegates #fragments to the storage engine' do
-      engine.should_receive(:fragments)
-      base.fragments
+      expect(engine).to receive(:fragments)
+      wrapper.fragments
     end
   end
 
@@ -34,9 +36,9 @@ describe Fragmenter::Wrapper do
       engine.stub('meta' => { 'content_type' => 'application/octet-stream' },
                   'fragments' => ['1', '2'])
 
-      base.as_json.tap do |json|
-        json.should have_key('content_type')
-        json.should have_key('fragments')
+      wrapper.as_json.tap do |json|
+        expect(json).to have_key('content_type')
+        expect(json).to have_key('fragments')
       end
     end
   end
